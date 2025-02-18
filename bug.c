a081729b17932524e01c56ec649202e68c0ba6cf
BugHunter-X <askzenai@gmail.com>
2025-02-18 23:34:05 +0100

    Add bug.c file

---
+++ bug.c
@@ -0,0 +1,1 @@
+int main() {    int x = 10;    int *ptr = &x;    *ptr = 20;    printf("%d\n", x); // Output: 20    free(ptr); //ERROR:  Attempting to free a pointer that was not allocated using malloc, calloc, or similar functions. This leads to undefined behavior. }
